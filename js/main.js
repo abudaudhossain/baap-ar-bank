60b151ced8a6f1e234e075d543cb9ca2596a11e3
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-15 00:05:29 +0600

    add login js function

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.getElementById('submit-btn').addEventListener('click', function(){
+    const userEmailFeild = document.getElementById('user-email');
+    const userEmail = userEmailFeild.value;
+
+    const userPasswordFeild = document.getElementById('user-password');
+    const userPassword = userPasswordFeild.value;
+
+    if(userEmail == 'sontan@baap.com' && userPassword == '123456'){
+        window.location.href ='baaper-bank.html';
+    }else{
+        alert('please enter valied info');
+    }
+    
+});
